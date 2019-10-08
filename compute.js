b3c147de02bcc335009e6a6ddc6b0db44db1a5df
Zahid Khawaja <zahidkhawaja@icloud.com>
2019-10-08 08:59:28 -0400

    Refactor compute.js to remove unnecessary variables

--- compute.js
+++ compute.js
@@ -2,14 +2,12 @@
 function convertToC() {
   let fValue = document.getElementById("fInputValue").value;
   let cTemp = Math.round((fValue - 32) / 1.8);
-  let cAnswer = `= ${cTemp} Celsius`;
-  document.getElementById("resultCValue").innerHTML = cAnswer;
+  document.getElementById("resultCValue").innerHTML = `= ${cTemp} Celsius`;
 }
 
 //Convert to Fahrenheit
 function convertToF() {
   let cValue = document.getElementById("cInputValue").value;
   let fTemp = Math.round((cValue * 1.8) + 32);
-  let fAnswer = `= ${fTemp} Fahrenheit`;
-  document.getElementById("resultFValue").innerHTML = fAnswer;
+  document.getElementById("resultFValue").innerHTML = `= ${fTemp} Fahrenheit`;
 }
